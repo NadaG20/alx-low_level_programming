f7f79c2baebf6a7eafd77a18665a8de8fac73002
NadaG20 <NadaMohamedGaber20@gmail.com>
2023-03-27 07:12:54 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,15 +2,15 @@
 
 /**
  * _puts - Prints a string
- * @str: The string to print
+ * @str: string to print
  * Return: void
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	while (*str != '\0')
 	{
-		_putchar(*str);
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
