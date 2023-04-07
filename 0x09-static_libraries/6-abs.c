946d28e404dc57328fe160832ec849fe1cbf7eb2
NadaG20 <NadaMohamedGaber20@gmail.com>
2023-04-07 08:27:41 -0700

    Updated

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absloutet value of an integer
+ * @n: the int to check
+ * Return: the absloute value of int
+ */
+
+int_abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
